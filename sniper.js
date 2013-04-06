5b17b78f6f5c2903281001aa849268bba1024248
John Tantalo <john.tantalo@gmail.com>
2013-04-05 19:44:28 -0700

    display radius around each agent

--- sniper.js
+++ sniper.js
@@ -5,9 +5,38 @@ var setAgents = function (num) {
   document.getElementById('agents').innerHTML = num;
 };
 
+function Agent(r) {
+  this.r = r;
+  this.alive = true;
+  this.circle = paper.circle();
+  this.range = paper.circle();
+};
+
+Agent.prototype.update = function () {
+  this.circle.attr('cx', this.x);
+  this.circle.attr('cy', this.y);
+  this.circle.attr('r', 3);
+  this.circle.attr('stroke-width', 0);
+  
+  
+  this.range.attr('cx', this.x);
+  this.range.attr('cy', this.y);
+  this.range.attr('r', this.r);
+  this.range.attr('fill', '#0000');
+  this.range.attr('stroke-width', 1);
+  
+  if (this.alive) {
+    this.circle.attr('fill', 'black');
+    this.range.attr('stroke', 'black')
+  } else {
+    this.circle.attr('fill', 'red');
+    this.range.attr('stroke', 'red')
+  }
+}
+
 var agents = [];
 for (var i = 0; i < 20; i++)
-  agents.push(paper.circle());
+  agents.push(new Agent(10));
 
 setAgents(agents.length);
 
@@ -19,41 +48,35 @@ function Sniper(r) {
 
 var sniper = new Sniper(150);
 
-var rkill = 20;
-
 paper.canvas.onclick = function(evt) {
   if (agents.length) {
     var agent = agents.pop();
     
-    var x = evt.offsetX, y = evt.offsetY;
-    // var a = paper.circle(x, y, 3).;
-    agent.attr('cx', x);
-    agent.attr('cy', y);
-    agent.attr('r', 3);
-    agent.attr('stroke-width', 0);
-    // debugger;
+    agent.x = evt.offsetX;
+    agent.y = evt.offsetY;
     
-    var dx = x - sniper.x;
-    var dy = y - sniper.y;
-    var r2 = dx * dx + dy * dy;
-    if (r2 < rkill * rkill) {
+    var dx = agent.x - sniper.x;
+    var dy = agent.y - sniper.y;
+    var r = Math.sqrt(dx * dx + dy * dy);
+    
+    if (r < agent.r) {
+      // Reveal the sniper. You win!
+      paper.circle(sniper.x, sniper.y, 3)
+          .attr('fill', 'red')
+          .attr('stroke-width', 0);
       paper.circle(sniper.x, sniper.y, sniper.r)
           .attr('fill', '#0000')
           .attr('stroke', 'red')
           .attr('stroke-width', 1);
-      paper.circle(sniper.x, sniper.y, rkill)
-          .attr('fill', '#0000')
-          .attr('stroke', 'black')
-          .attr('stroke-width', 1);
-      agent.attr('fill', 'black');
-      agents.unshift(agent);
-    } else if (r2 < sniper.r * sniper.r) {
-      agent.attr('fill', 'red');
+      agents.length = 0;
+    } else if (r < sniper.r) {
+      // This agent is dead.
+      agent.alive = false;
     } else {
-      agent.attr('fill', 'black');
       agents.unshift(agent);
     }
-        
+    
+    agent.update();
     setAgents(agents.length);
   }
 };
